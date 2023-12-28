5e75b7b46613ec7ea69b00b4b72438dbace8c2d9
peterhorvat <peter.horvat85@gmail.com>
2023-12-28 15:04:32 +0100

    Added support for account summary

--- main.py
+++ main.py
@@ -9,22 +9,28 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Generate PDF from JSON data and template.')
     parser.add_argument('json_file', type=str, help='Path to the JSON file')
     parser.add_argument('template_path', type=str, help='Path to the template file (e.g., transactions.html)')
-    parser.add_argument('--style_path', type=str, nargs='?', default='base.css', help='Path to the style file (e.g., base.css)')
+    parser.add_argument('--style_path', type=str, nargs='?', default='base.css',
+                        help='Path to the style file (e.g., base.css)')
     parser.add_argument('--date_from', type=str, nargs='?', default='2020-03-04', help='Date from (e.g., 2020-03-04)')
     parser.add_argument('--date_to', type=str, nargs='?', default='2022-04-04', help='Date to (e.g., 2020-03-04)')
+    parser.add_argument('--summary', default=False, action='store_true',
+                        help='Relevant only for Crypto Rransaction Report')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_arguments()
-
     records = load_data_from_json(args.json_file)
+    summary = load_data_from_json('data/summary.json')
+
     content = {
         'header': True,
         'footer': True,
         'date_from': args.date_from,
         'date_to': args.date_to,
         'records': records,
+        'summary': summary if args.summary else None,
+        'fiat_currency': 'EUR'
     }
     pdf = generate_pdf(args.template_path, args.style_path, content)
     create_directory('out')
